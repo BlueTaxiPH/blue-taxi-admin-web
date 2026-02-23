9fec4836305b046c227a0fed07b650f358be8e17
candace1818 <candaceyu1818@gmail.com>
2026-02-23 02:55:59 -0500

    feat: added components

---
+++ components/icons/lock-icon.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils"
+
+function LockIcon({ className, ...props }: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("size-4", className)}
+      {...props}
+    >
+      <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+      <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+    </svg>
+  )
+}
+
+export { LockIcon }
